7b3e93641517c323ef9606dbb71d1d77a9c5656c
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-03-25 14:44:20 +0600

    Added Wordpress and Heading 2

--- wp api/wpapi.py
+++ wp api/wpapi.py
@@ -1,5 +1,12 @@
-import requests
-from .errors import WpApiError
+class WpApi():
 
-class WpAPI():
-    pass
+    def wpp(text):
+        wp_paragaraph = '<!-- wp:paragraph --><p>'+text+'</p><!-- /wp:paragraph -->'
+        return wp_paragaraph
+    
+
+    def wph2(variable):
+        heading2 = '<!-- wp:heading --><h2>' + variable + '</h2><!-- /wp:heading -->'
+        return heading2
+    
+    
